eb5ca5820a163bc76cc5121d9d661286a157205c
Marco Estrella <mestrella.1991@gmail.com>
2021-03-18 22:32:26 +1100

    Added dependencies: SQLDelight, Ktor, Coroutines and Serialization

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TheMoviesKMM
+
+## sqldelight
+
+https://github.com/cashapp/sqldelight
+https://cashapp.github.io/sqldelight/multiplatform_sqlite/
+https://plugins.jetbrains.com/plugin/8191-sqldelight
+
+## Ktor
+
+https://ktor.io/docs/http-client-multiplatform.html
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 buildscript {
+
+    val kotlinVersion = "1.4.0"
+    val sqlDelightVersion = "1.4.4"
+
     repositories {
         gradlePluginPortal()
         jcenter()
@@ -6,8 +10,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
         classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
+        classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
     }
 }
 
