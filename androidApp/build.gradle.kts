db4dbb618656e5d2d5aabc2ecb7820e8339aaadc
Marco Estrella <jmarkstar.1991@gmail.com>
2021-05-04 20:10:46 +1000

    Added android client.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -8,10 +8,18 @@ dependencies {
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.recyclerview:recyclerview:1.2.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
 
 android {
+
     compileSdkVersion(29)
+
     defaultConfig {
         applicationId = "com.jmarkstar.movies.androidApp"
         minSdkVersion(24)
@@ -19,9 +27,19 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
         }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 }
